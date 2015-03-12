dc38344125273607bed3a04ecfeaec9cb6d9888b
Liviu <enzime4u@yahoo.com>
2015-03-12 14:27:08 +0200

    finished

--- js/app.js
+++ js/app.js
@@ -14,65 +14,40 @@ $(function() {
     
      
    
-//when the page is loading , or when we press newGame generate a random number
-//the number must be between 1 and 100 
-function generateNumber() {
-  var randomNumber = Math.floor((Math.random()*100)+1);
-  console.log("Generated Random Number = " +randomNumber);
-  return randomNumber;
- }
+    //when the page is loading , or when we press newGame generate a random number
+    //the number must be between 1 and 100 
+     function generateNumber() {
+        var randomNumber = Math.floor((Math.random()*100)+1);  //generates a decimal between 0 and 1 and then we add 1 , maybe is 0 
+        console.log("Generated Random Number =" +randomNumber);
+        return randomNumber;
+     }
 
-var randomNumber = generateNumber(); 
+    var randomNumber = generateNumber(); 
     
     //guessNumber
-$('#guessButton').on('click', function() {      // on CLICK on the Guess button 
-       var guessedNumber =  $('#userGuess').val();
-  if ( isNaN(guessedNumber) || guessedNumber < 1 || guessedNumber > 100 ) { //check if the input is a NUMBER and if it is bewtween 1 and 100 , if is not ... alert and error
-       guessedNumber = Number( $(".overlay").fadeIn(1000) ); // the  overlay class appears with the red big box with instructions
-       }       
-console.log(guessedNumber); //we are printing to the console the guessed number
-
- 
-  $('#userGuess').val('');// reseting the input area. 
-  $('#guessList').append('<li>'+ guessedNumber +'</li>');  //add the guessNumber to the list ( append() )
-  diffNumber();        
-})
-
-$('form').submit(function(event) {
-      event.preventDefault(); 
- })
+    //in the input area , the user enters a number and press enter
+    //or click guess.
+   
+    
+    //check if the input is a number, check if is between 1 and 100,
+    //if  is not a number or is not between 1 and 100 , we should give an 
+    //error: ' Please enter a number between 1 and 100 '
 
 
+    //differenceNumber
+    //calculate the Difference between the Guess and Random Number
 
 
-//differenceNumber
-//calculate the Difference between the Guess and Random Number
-function diffNumber() {
-  var differenceNumber = randomNumber - guessedNumber;
-  if (differenceNumber < 0 ){
-    differenceNumber = Math.abs(differenceNumber);
-  } 
-  
-}
-
-
-
-
-
-
-//we need to give feedback to the user if his guessNumber is close of far from the 
-//randomNumber  ( difference is big or small)
+    //we need to give feedback to the user if his guessNumber is close of far from the 
+    //randomNumber  ( difference is big or small)
     
 
-
-//countNumber - counts how many guesses  ( add 1 to the count )
- 
+    //countNumber - counts how many guesses  ( add 1 to the count )
 
 
 
 
-
-
+    //add the guessNumber to the list ( append() )
 
 });
 
--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,7 @@
 $(function() {
     var randomNumber;
     var differenceNumber;
+    var guessCounter = 0; //guessCounter set to 0;
 
     /*--- Display information modal box ---*/
     $(".what").click(function(event) {
@@ -26,33 +27,22 @@ $(function() {
         if (isNaN(guessedNumber) || guessedNumber < 1 || guessedNumber > 100) {
             // the  overlay class appears with the red big box with instructions
             guessedNumber = Number($(".overlay").fadeIn(1000));
-        }
+            $('#userGuess').val('');
+            return false;
+        } 
         console.log(guessedNumber);
-
+        diffNumber(guessedNumber);
         //reseting the input area.
         $('#userGuess').val('');
         //adding the guessedNumber to the list - append() 
         $('#guessList').append('<li>' + guessedNumber + '</li>');
+        // count how many guesses
+        guessCounter++; 
+        $('#count').text(guessCounter);
 
     })
-
-
-    //we need to give feedback to the user (hot, cold etc)
-    if (differenceNumber >= 50) {
-        console.log('Ice Cold');
-    } else if (differenceNumber <= 50 && differenceNumber >= 40) {
-        console.log('Cold');
-    } else if (differenceNumber <= 40 && differenceNumber >= 30) {
-        console.log();
-    } else if (differenceNumber <= 30 && differenceNumber >= 20) {
-        console.log();
-    };
-
+    
 });
-
-
-// count how many guesses.
-var countGuesses = 0;
 
 //function New Game ---//
 function newGame() {
@@ -74,10 +64,25 @@ function newGame() {
     }
 
 //differenceNumber - difference from guessedNumber and randomNumber
-function diffNumber() {
+function diffNumber(guessedNumber) {
     differenceNumber = randomNumber - guessedNumber;
     if (differenceNumber < 0) {
         differenceNumber = Math.abs(differenceNumber);
-        console.log(differenceNumber);
+     console.log(differenceNumber);
+    }
+    //we need to give feedback to the user (hot, cold etc)
+    if (differenceNumber >= 50) {
+        $('#feedback').html('ICE COLD');
+    } else if (differenceNumber <= 50 && differenceNumber >= 30) {
+        $('#feedback').html('COLD');
+    } else if (differenceNumber <= 30 && differenceNumber >= 20) {
+        $('#feedback').html('WARM');
+    } else if (differenceNumber <= 20 && differenceNumber >= 10) {
+        $('#feedback').html('HOT');
+    } else if (differenceNumber <= 10 && differenceNumber >= 1) {
+        $('#feedback').html('VERY HOT ');
+    } else if (differenceNumber == 0) {
+        $('#feedback').html('Congratulatiosn, you guessed the number: ' + randomNumber);
+        $('#userGuess, #guessButton').prop('disabled',true);
     }
 }
